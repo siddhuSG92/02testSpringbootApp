935e82014981c5e99030ac4491d354741228dc14
siddhu.sg92@gmail.com <siddhu.sg92@gmail.com>
2023-03-19 15:56:01 +0800

    initial commit demo 2

---
+++ src/main/java/com/grsTech/grsCart/SpringbootGitDemo2Application.java
@@ -0,0 +1,13 @@
+package com.grsTech.grsCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitDemo2Application.class, args);
+	}
+
+}
