54cf43c86b7997526a309fe63060a5070b278f59
siddhu.sg92@gmail.com <siddhu.sg92@gmail.com>
2023-03-19 16:14:16 +0800

    modified main class

--- src/main/java/com/grsTech/grsCart/SpringbootGitDemo2Application.java
+++ src/main/java/com/grsTech/grsCart/SpringbootGitDemo2Application.java
@@ -2,12 +2,14 @@ package com.grsTech.grsCart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootGitDemo2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootGitDemo2Application.class, args);
+	ConfigurableApplicationContext run = SpringApplication.run(SpringbootGitDemo2Application.class, args);
+	run.close(); 
 	}
 
 }
